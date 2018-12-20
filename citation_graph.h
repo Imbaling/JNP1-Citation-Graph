b79f836d92a74c08bf9b726f500eb735e1f0faa3
Imbaling <marek@zochowski.net>
2018-12-20 22:38:39 +0100

    Improve style

--- citation_graph.h
+++ citation_graph.h
@@ -41,7 +41,8 @@ class CitationGraph {
                 typename map_type::iterator map_iterator;
 
                 std::shared_ptr<Publication> pointer;
-                std::set<std::shared_ptr<Node>, std::owner_less<std::shared_ptr<Node>>> children;
+                std::set<std::shared_ptr<Node>,
+												 std::owner_less<std::shared_ptr<Node>>> children;
 								std::vector<std::weak_ptr<Node>> parents;
 
             public:
@@ -53,11 +54,11 @@ class CitationGraph {
 
                 Node(const Node &) = delete;
 
-                void setMapIterator(typename map_type::iterator const &it) {
+                void set_map_iterator(typename map_type::iterator const &it) {
                     map_iterator = it;
                 }
 
-                Publication& getPublication() const noexcept {
+                Publication& get_publication() const noexcept {
                     return *pointer;
                 }
 
@@ -65,47 +66,47 @@ class CitationGraph {
                     return children.find(node) != children.end();
                 }
 
-		        		void addChild(std::shared_ptr<Node> const &node) {
+		        		void add_child(std::shared_ptr<Node> const &node) {
 		          			children.insert(node);
 		        		}
 
-		        		void addParent(std::weak_ptr<Node> const &node) {
+		        		void add_parent(std::weak_ptr<Node> const &node) {
 		          			parents.push_back(node);
 		        		}
 
-								void removeLastParent() noexcept {
+								void remove_last_parent() noexcept {
 										if (!parents.empty())
 												parents.pop_back();
 								}
 
-		        		std::vector<id_type> getChildrenIds() const {
-			        			std::vector<id_type> childrenIds;
+		        		std::vector<id_type> get_children_ids() const {
+			        			std::vector<id_type> children_ids;
 
                     for (const auto &child : children) {
-                        childrenIds.push_back(child->getPublication().get_id());
+                        children_ids.push_back(child->get_publication().get_id());
                     }
 
-                    return childrenIds;
+                    return children_ids;
 		        		}
 
-		        		std::vector<id_type> getParentsIds() const {
-	                	std::vector<id_type> parentsIds;
+		        		std::vector<id_type> get_parents_ids() const {
+	                	std::vector<id_type> parents_ids;
 
                     for (const auto &parent : parents) {
 												if (parent.expired())
 														continue;
 
-                        parentsIds.push_back(parent.lock()->getPublication().get_id());
+                        parents_ids.push_back(parent.lock()->get_publication().get_id());
                     }
 
-                    return parentsIds;
+                    return parents_ids;
 		        		}
 
 								void erase(const std::shared_ptr<Node> &son) noexcept {
 										children.erase(son);
 								}
 
-                void removeNode(const std::shared_ptr<Node> &me) {
+                void remove_node(const std::shared_ptr<Node> &me) {
                     for (const auto &parent : parents) {
                         	parent.lock()->erase(me);
                     }
@@ -126,9 +127,10 @@ class CitationGraph {
         CitationGraph(id_type const &stem_id)
             : publications(std::make_unique<map_type>()),
               root(std::make_shared<Node>(publications.get(), stem_id)) {
-            const auto it = publications->insert(typename map_type::value_type(stem_id, root));
+            const auto it =
+								publications->insert(typename map_type::value_type(stem_id, root));
 
-            root->setMapIterator(it.first);
+            root->set_map_iterator(it.first);
         }
 
         // Move constructor.
@@ -147,130 +149,133 @@ class CitationGraph {
 
         // Returns id of the root.
         id_type get_root_id() const noexcept(noexcept(std::declval<Publication>().get_id())) {
-            return root->getPublication().get_id();
+            return root->get_publication().get_id();
         }
 
         // Returns list of publications's ids citing the given publication.
         // Throws PublicationNotFound exception if the given publication does
         // not exist.
-        std::vector<typename Publication::id_type> get_children(typename Publication::id_type const &id) const {
+        std::vector<id_type> get_children(id_type const &id) const {
             const auto publication = publications->find(id);
 
             if (publication == publications->end()) {
                 throw PublicationNotFound();
             }
 
-            return publication->second.lock()->getChildrenIds();
+            return publication->second.lock()->get_children_ids();
     		}
 
         // Returns list of publications's ids cited by the given publication.
         // Throws PublicationNotFound exception if the given publication does
         // not exist.
-        std::vector<typename Publication::id_type> get_parents(typename Publication::id_type const &id) const {
+        std::vector<id_type> get_parents(id_type const &id) const {
             const auto publication = publications->find(id);
 
             if (publication == publications->end()) {
                 throw PublicationNotFound();
             }
 
-            return publication->second.lock()->getParentsIds();
+            return publication->second.lock()->get_parents_ids();
     		}
 
         // Check if the given publication exists.
-        bool exists(typename Publication::id_type const &id) const {
+        bool exists(id_type const &id) const {
     			  return publications->find(id) != publications->end();
     	  }
 
         // Returns refference to a representation of publication of the given id.
         // Throws PublicationNotFound exception if the given publication does
         // not exist.
-        Publication& operator[](typename Publication::id_type const &id) const {
+        Publication& operator[](id_type const &id) const {
       		const auto publication = publications->find(id);
 
             if (publication == publications->end())
                 throw PublicationNotFound();
 
-      			return publication->second.lock()->getPublication();
+      			return publication->second.lock()->get_publication();
     		}
-        
-        // Creates node representing new publication which cites publication 
+
+        // Creates node representing new publication which cites publication
         // identified by parent_id or publications identified by vector parents_id.
-        // Throws exception PublicationAlreadyCreated if publication identified by id 
-        // doesen`t exist. Throws exception PublicationNotFound if one of the arguments 
-        // doesen`t exist or vector is empty.
-				void create(typename Publication::id_type const &id, typename std::vector<typename Publication::id_type> const &parent_ids) {
+        // Throws exception PublicationAlreadyCreated if publication identified by id
+        // doesn`t exist. Throws exception PublicationNotFound if one of the arguments
+        // doesn`t exist or vector is empty.
+				void create(id_type const &id, typename std::vector<id_type> const &parent_ids) {
 						const auto child_node = publications->find(id);
 						if (child_node != publications->end())
-							throw PublicationAlreadyCreated();
+								throw PublicationAlreadyCreated();
 
-						for (auto parent_id : parent_ids) {
+						for (const auto &parent_id : parent_ids) {
 								const auto parent_node = publications->find(parent_id);
 								if (parent_node == publications->end())
 										throw PublicationNotFound();
 						}
 
+						if (parent_ids.empty())
+								throw PublicationNotFound();
+
 						const auto node = std::make_shared<Node>(publications.get(), id);
 						const auto it = publications->insert(typename map_type::value_type(id, node));
 
 						try {
-					      node->setMapIterator(it.first);
+					      node->set_map_iterator(it.first);
 						} catch (...) {
 								publications->erase(it.first);
 								throw;
 						}
 
-						for (auto parent_id : parent_ids) {
+						for (const auto &parent_id : parent_ids) {
 								try {
 										add_citation(id, parent_id);
-								} catch(std::exception &e) {
-									  node->removeNode(node);
-										throw e;
+								} catch(...) {
+									  node->remove_node(node);
+										throw;
 								}
 						}
 				}
 
-        void create(typename Publication::id_type const &id, typename Publication::id_type const &parent_id) {
-						std::vector<typename Publication::id_type> parents_ids = {parent_id};
+        void create(id_type const &id, id_type const &parent_id) {
+						std::vector<id_type> parents_ids = {parent_id};
 
 						create(id, parents_ids);
 				}
 
-        // Adds new edge in the Citation Graph. Throws exception 
-        // PublicationNotFound if one of the publications doesen`t exist.
-        void add_citation(typename Publication::id_type const &child_id, typename Publication::id_type const &parent_id) {
+        // Adds new edge in the Citation Graph. Throws exception
+        // PublicationNotFound if one of the publications doesn`t exist.
+        void add_citation(id_type const &child_id, id_type const &parent_id) {
 	        	auto child_publication = publications->find(child_id);
 	        	auto parent_publication = publications->find(parent_id);
 
 	        	if (child_publication != publications->end() && parent_publication != publications->end()) {
-					if (parent_publication->second.lock()->exists(child_publication->second.lock()) == false) {
-					   child_publication->second.lock()->addParent(parent_publication->second);
-                    
-                       try {
-						  parent_publication->second.lock()->addChild(child_publication->second.lock());
-					   } catch(std::exception &e) {
-						  child_publication->second.lock()->removeLastParent();
-						  throw e;
-					   }
-                    }
+								if (parent_publication->second.lock()->exists(child_publication->second.lock()) == false) {
+								   child_publication->second.lock()->add_parent(parent_publication->second);
+
+			             try {
+										  parent_publication->second.lock()->add_child(child_publication->second.lock());
+								   } catch(...) {
+										  child_publication->second.lock()->remove_last_parent();
+										  throw;
+								   }
+			          }
 	        	} else {
 	          		throw PublicationNotFound();
 	        	}
       	}
 
         // Removes publication identified by id. Throws exception PublicationNotFound if the
-        // Publication doesen`t exist. Throws exception TriedToremoveRoot while attempting
+        // publication doesn`t exist. Throws exception TriedToremoveRoot while attempting
         // to remove the first publication. In the case of the disconnectedness of the graph
-        // we leave only the connected component.
-        void remove(typename Publication::id_type const &id) {
+        // we leave only the component connected to the root.
+        void remove(id_type const &id) {
 	      		auto publication = publications->find(id);
 
             if (publication == publications->end())
                 throw PublicationNotFound();
 
-            if (root->getPublication().get_id() == id)
+            if (root->get_publication().get_id() == id)
                 throw TriedToRemoveRoot();
 
-            publication->second.lock()->removeNode(publication->second.lock());
+            publication->second.lock()->remove_node(publication->second.lock());
     	}
 };
 
